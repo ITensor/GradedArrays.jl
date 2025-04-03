ac5938f820fd06d7178e55be6afed95b052a00f3
mtfishman <mfishman@flatironinstitute.org>
2025-04-03 09:11:58 -0400

    Add more tests and functionality

--- src/GradedArrays.jl
+++ src/GradedArrays.jl
@@ -3,8 +3,11 @@ module GradedArrays
 include("lib/LabelledNumbers/LabelledNumbers.jl")
 using .LabelledNumbers: LabelledNumbers
 include("lib/GradedUnitRanges/GradedUnitRanges.jl")
+# TODO: Load `gradedrange`, `dual`, etc.
 using .GradedUnitRanges: GradedUnitRanges
 include("lib/SymmetrySectors/SymmetrySectors.jl")
 using .SymmetrySectors: SymmetrySectors
+include("gradedarray.jl")
+include("tensoralgebra.jl")
 
 end
