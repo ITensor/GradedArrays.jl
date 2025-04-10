3f8b70956a6f193e7ccb85b66f58142d1ed40a8f
Olivier Gauthé <ogauthe@flatironinstitute.org>
2025-04-10 11:00:52 -0400

    matricize_axes and sectormergesort

--- ext/GradedArraysTensorAlgebraExt/GradedArraysTensorAlgebraExt.jl
+++ ext/GradedArraysTensorAlgebraExt/GradedArraysTensorAlgebraExt.jl
@@ -26,16 +26,13 @@ TensorAlgebra.FusionStyle(::Type{<:GradedArray}) = SectorFusion()
 # TODO consider heterogeneous sectors?
 TensorAlgebra.trivial_axis(t::Tuple{Vararg{AbstractGradedUnitRange}}) = trivial(first(t))
 
-fuse_or_trivial_axis(axes::Tuple, ::Tuple) = unmerged_tensor_product(axes...)
-fuse_or_trivial_axis(::Tuple{}, axes::Tuple) = trivial_axis(axes)
-
-function row_and_column_axes(
+function matricize_axes(
   blocked_axes::BlockedTuple{2,<:Any,<:Tuple{Vararg{AbstractUnitRange}}}
 )
   codomain_axes, domain_axes = blocks(blocked_axes)
   @assert !(isempty(codomain_axes) && isempty(domain_axes))
-  row_axis = fuse_or_trivial_axis(codomain_axes, domain_axes)
-  unflipped_col_axis = fuse_or_trivial_axis(domain_axes, codomain_axes)
+  row_axis = unmerged_tensor_product(trivial_axes(domain_axes), codomain_axes...)
+  unflipped_col_axis = unmerged_tensor_product(trivial_axes(codomain_axes), domain_axes...)
   return row_axis, flip(unflipped_col_axis)
 end
 
@@ -43,10 +40,10 @@ function TensorAlgebra.matricize(
   ::SectorFusion, a::AbstractArray, biperm::AbstractBlockPermutation{2}
 )
   a_perm = permutedims(a, Tuple(biperm))
-  row_axis, col_axis = row_and_column_axes(axes(a)[biperm])
+  row_axis, col_axis = matricize_axes(axes(a)[biperm])
   a_reshaped = blockreshape(a_perm, (row_axis, col_axis))
   # Sort the blocks by sector and merge the equivalent sectors.
-  return block_mergesort(a_reshaped)
+  return sectormergesort(a_reshaped)
 end
 
 function TensorAlgebra.unmatricize(
@@ -71,7 +68,7 @@ function TensorAlgebra.unmatricize(
 end
 
 # Sort the blocks by sector and then merge the common sectors.
-function block_mergesort(a::AbstractArray)
+function sectormergesort(a::AbstractArray)
   I = blockmergesortperm.(axes(a))
   return a[I...]
 end
