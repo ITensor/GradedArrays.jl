97ef71fafdd9d5fd929733a72379bb9d71085ced
Lukas Devos <ldevos98@gmail.com>
2025-09-25 12:05:30 -0400

    fix tests

--- test/test_sectorunitrange.jl
+++ test/test_sectorunitrange.jl
@@ -11,7 +11,7 @@ using BlockArrays:
   findblock
 using GradedArrays:
   U1,
-  SU,
+  SU2,
   SectorOneTo,
   SectorUnitRange,
   SectorVector,
@@ -31,68 +31,68 @@ using Test: @test, @test_throws, @testset
 using TestExtras: @constinferred
 
 @testset "SectorUnitRange" begin
-  sr = sectorrange(SU((1, 0)), 2)
+  sr = sectorrange(SU2(1/2), 2)
   @test sr isa SectorUnitRange
 
   # accessors
-  @test sector(sr) == SU((1, 0))
+  @test sector(sr) == SU2(1/2)
   @test ungrade(sr) isa Base.OneTo
-  @test ungrade(sr) == 1:6
+  @test ungrade(sr) == 1:4
   @test !isdual(sr)
 
   # Base interface
   @test first(sr) == 1
-  @test last(sr) == 6
-  @test length(sr) == 6
+  @test last(sr) == 4
+  @test length(sr) == 4
   @test firstindex(sr) == 1
-  @test lastindex(sr) == 6
+  @test lastindex(sr) == 4
   @test eltype(sr) ≡ Int
   @test step(sr) == 1
-  @test eachindex(sr) == Base.oneto(6)
+  @test eachindex(sr) == Base.oneto(4)
   @test only(axes(sr)) isa SectorOneTo
   @test sector(only(axes(sr))) == sector(sr)
-  @test only(axes(sr)) == 1:6
+  @test only(axes(sr)) == 1:4
   @test iterate(sr) == (1, 1)
-  for i in 1:5
+  for i in 1:3
     @test iterate(sr, i) == (i + 1, i + 1)
   end
-  @test isnothing(iterate(sr, 6))
+  @test isnothing(iterate(sr, 4))
 
   # Base.Slice
   @test axes(Base.Slice(sr)) ≡ (sr,)
   @test Base.axes1(Base.Slice(sr)) ≡ sr
   @test Base.unsafe_indices(Base.Slice(sr)) ≡ (sr,)
 
-  @test sr == 1:6
+  @test sr == 1:4
   @test sr == sr
   @test space_isequal(sr, sr)
 
-  sr = sectorrange(SU((1, 0)) => 2)
+  sr = sectorrange(SU2(1/2) => 2)
   @test sr isa SectorUnitRange
-  @test sector(sr) == SU((1, 0))
+  @test sector(sr) == SU2(1/2)
   @test ungrade(sr) isa Base.OneTo
-  @test ungrade(sr) == 1:6
+  @test ungrade(sr) == 1:4
   @test !isdual(sr)
 
-  sr = sectorrange(SU((1, 0)) => 2, true)
+  sr = sectorrange(SU2(1/2) => 2, true)
   @test sr isa SectorUnitRange
-  @test sector(sr) == SU((1, 0))
+  @test sector(sr) == SU2(1/2)
   @test ungrade(sr) isa Base.OneTo
-  @test ungrade(sr) == 1:6
+  @test ungrade(sr) == 1:4
   @test isdual(sr)
 
-  sr = sectorrange(SU((1, 0)), 4:10, true)
+  sr = sectorrange(SU2(1/2), 4:10, true)
   @test sr isa SectorUnitRange
-  @test sector(sr) == SU((1, 0))
+  @test sector(sr) == SU2(1/2)
   @test ungrade(sr) isa UnitRange
   @test ungrade(sr) == 4:10
   @test isdual(sr)
 
-  sr = sectorrange(SU((1, 0)), 2)
-  @test !space_isequal(sr, sectorrange(SU((1, 1)), 2))
-  @test !space_isequal(sr, sectorrange(SU((1, 0)), 2:7))
-  @test !space_isequal(sr, sectorrange(SU((1, 1)), 2, true))
-  @test !space_isequal(sr, sectorrange(SU((1, 0)), 2, true))
+  sr = sectorrange(SU2(1/2), 2)
+  @test !space_isequal(sr, sectorrange(SU2(1), 2))
+  @test !space_isequal(sr, sectorrange(SU2(1/2), 2:7))
+  @test !space_isequal(sr, sectorrange(SU2(1), 2, true))
+  @test !space_isequal(sr, sectorrange(SU2(1/2), 2, true))
 
   sr2 = copy(sr)
   @test sr2 isa SectorUnitRange
@@ -104,36 +104,36 @@ using TestExtras: @constinferred
   # BlockArrays interface
   @test blockaxes(sr) isa Tuple{BlockRange{1,<:Tuple{Base.OneTo}}}
   @test space_isequal(sr[Block(1)], sr)
-  @test only(blocklasts(sr)) == 6
+  @test only(blocklasts(sr)) == 4
   @test findblock(sr, 2) == Block(1)
 
   @test blocklength(sr) == 1
-  @test blocklengths(sr) == [6]
-  @test only(blocks(sr)) == 1:6
+  @test blocklengths(sr) == [4]
+  @test only(blocks(sr)) == 1:4
   @test blockisequal(sr, sr)
 
   # GradedUnitRanges interface
-  @test sector_type(sr) ≡ SU{3,2}
-  @test sector_type(typeof(sr)) ≡ SU{3,2}
-  @test sectors(sr) == [SU((1, 0))]
+  @test sector_type(sr) ≡ SU2
+  @test sector_type(typeof(sr)) ≡ SU2
+  @test sectors(sr) == [SU2(1/2)]
   @test sector_multiplicity(sr) == 2
   @test sector_multiplicities(sr) == [2]
-  @test quantum_dimension(sr) == 6
+  @test quantum_dimension(sr) == 4
 
   srd = dual(sr)
-  @test sector(srd) == SU((1, 0))
-  @test space_isequal(srd, sectorrange(SU((1, 0)), 2, true))
-  @test sectors(srd) == [SU((1, 0))]
+  @test sector(srd) == SU2(1/2)
+  @test space_isequal(srd, sectorrange(SU2(1/2), 2, true))
+  @test sectors(srd) == [SU2(1/2)]
 
   srf = flip(sr)
-  @test sector(srf) == SU((1, 1))
-  @test space_isequal(srf, sectorrange(SU((1, 1)), 2, true))
+  @test sector(srf) == SU2(1/2)
+  @test space_isequal(srf, sectorrange(SU2(1/2), 2, true))
 
   # getindex
   @test_throws BoundsError sr[0]
   @test_throws BoundsError sr[7]
   @test (@constinferred getindex(sr, 1)) isa Int64
-  for i in 1:6
+  for i in 1:4
     @test sr[i] == i
   end
   @test sr[2:3] == 2:3
@@ -143,10 +143,10 @@ using TestExtras: @constinferred
 
   sr2 = (@constinferred getindex(sr, (:, 2)))
   @test sr2 isa SectorUnitRange
-  @test space_isequal(sr2, sectorrange(SU((1, 0)), 4:6))
+  @test space_isequal(sr2, sectorrange(SU2(1/2), 3:4))
   sr3 = (@constinferred getindex(sr, (:, 1:2)))
   @test sr3 isa SectorUnitRange
-  @test space_isequal(sr3, sectorrange(SU((1, 0)), 1:6))
+  @test space_isequal(sr3, sectorrange(SU2(1/2), 1:4))
 
   # Abelian slicing
   srab = sectorrange(U1(1), 3)
