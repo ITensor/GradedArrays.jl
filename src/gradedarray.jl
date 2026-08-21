09d1108f59b6c00e3bc46919face88e0b7a6b292
Matthew Fishman <mfishman@flatironinstitute.org>
2026-08-20 21:22:44 -0400

    Split the rmul! and lmul! bodies onto separate lines

--- src/gradedarray.jl
+++ src/gradedarray.jl
@@ -432,10 +432,12 @@ end
 TensorAlgebra.zero!(fa::GradedArray) = (zero!(matricize(fa)); fa)
 TensorAlgebra.scale!(fa::GradedArray, α::Number) = (scale!(matricize(fa), α); fa)
 function LinearAlgebra.rmul!(fa::GradedArray, α::Number)
-    return (LinearAlgebra.rmul!(matricize(fa), α); fa)
+    LinearAlgebra.rmul!(matricize(fa), α)
+    return fa
 end
 function LinearAlgebra.lmul!(α::Number, fa::GradedArray)
-    return (LinearAlgebra.lmul!(α, matricize(fa)); fa)
+    LinearAlgebra.lmul!(α, matricize(fa))
+    return fa
 end
 LinearAlgebra.norm(fa::GradedArray, p::Real = 2) = LinearAlgebra.norm(matricize(fa), p)
 Base.fill!(fa::GradedArray, v) = (fill!(matricize(fa), v); fa)
